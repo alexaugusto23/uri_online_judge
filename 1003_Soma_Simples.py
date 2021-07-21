e1b9ea06dcb28dce0c06a9cb862d17f6a58d2b44
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-07-20 21:58:56 -0300

    1000 ao 1003

---
+++ 1003_Soma_Simples.py
@@ -0,0 +1,40 @@
+"""
+
+Leia dois valores inteiros, no caso para variáveis A e B. A seguir, calcule a soma entre elas e atribua à variável SOMA. A seguir escrever o valor desta variável.
+
+Entrada
+O arquivo de entrada contém 2 valores inteiros.
+
+Saída
+Imprima a mensagem "SOMA" com todas as letras maiúsculas, com um espaço em branco antes e depois da igualdade seguido pelo valor correspondente à soma de A e B. Como todos os problemas, não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+
+
+Exemplos de Entrada:	
+30
+10
+
+-30
+10
+
+0
+0
+
+Exemplos de Saída:
+
+SOMA = 40
+
+SOMA = -20
+
+SOMA = 0
+
+
+"""
+## Code:##
+
+a, b = int(input()), int(input())
+
+def soma(a, b):
+    soma = int(a) + int(b)
+    return f"SOMA = {soma}"
+
+print(soma(a,b))
