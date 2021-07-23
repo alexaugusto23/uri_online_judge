cdeddb188b6d25dade7a9ade9496d6e1e18f51dd
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-07-23 18:10:54 -0300

    segunda página de desafios

--- 1036_Formula_Bhaskara.py
+++ 1036_Formula_Bhaskara.py
@@ -1,3 +1,47 @@
 """
+
+Leia 3 valores de ponto flutuante e efetue o cálculo das raízes da equação de Bhaskara. Se não for possível calcular as raízes, mostre a mensagem correspondente “Impossivel calcular”, caso haja uma divisão por 0 ou raiz de numero negativo.
+
+Entrada
+Leia três valores de ponto flutuante (double) A, B e C.
+
+Saída
+Se não houver possibilidade de calcular as raízes, apresente a mensagem "Impossivel calcular". Caso contrário, imprima o resultado das raízes com 5 dígitos após o ponto, com uma mensagem correspondente conforme exemplo abaixo. Imprima sempre o final de linha após cada mensagem.
+
+Exemplos de Entrada:
+10.0 20.1 5.1
+0.0 20.0 5.0
+10.3 203.0 5.0
+10.0 3.0 5.0
+
+Exemplos de Saída:
+
+R1 = -0.29788
+R2 = -1.71212
+
+Impossivel calcular
+
+R1 = -0.02466
+R2 = -19.68408
+
+Impossivel calcular
+
 """
 ## Code: ##
+
+from math import sqrt
+
+A,B,C = list(map(float, input().split()))
+
+# Fórmula de Bhaskara
+
+def bhaskara(A,B,C):
+    try:
+        x1 = (-B + sqrt(B**2 - 4 * A * C)) / (2 * A)
+        x2 = (-B - sqrt(B**2 - 4 * A * C)) / (2 * A)
+        return f"R1 = {x1:.5f}\nR2 = {x2:.5f}"
+
+    except ZeroDivisionError: 
+        return f"Impossivel calcular"
+    except ValueError: return f"Impossivel calcular"
+print(bhaskara(A,B,C))
--- 1037_intervalo.py
+++ 1037_intervalo.py
@@ -1,3 +1,41 @@
 """
+Você deve fazer um programa que leia um valor qualquer e apresente uma mensagem dizendo em qual dos seguintes intervalos ([0,25], (25,50], (50,75], (75,100]) este valor se encontra. Obviamente se o valor não estiver em nenhum destes intervalos, deverá ser impressa a mensagem “Fora de intervalo”.
+
+O símbolo ( representa "maior que". Por exemplo:
+[0,25]  indica valores entre 0 e 25.0000, inclusive eles.
+(25,50] indica valores maiores que 25 Ex: 25.00001 até o valor 50.0000000
+
+Entrada
+O arquivo de entrada contém um número com ponto flutuante qualquer.
+
+Saída
+A saída deve ser uma mensagem conforme exemplo abaixo.
+
+Exemplo de Entrada:	
+
+25.01
+25.00
+100.00
+-25.02
+
+Exemplo de Saída:
+
+Intervalo (25,50]
+Intervalo [0,25]
+Intervalo (75,100]
+Fora de intervalo
 """
 ## Code: ##
+
+intervalor_print = ('[0,25]', '(25,50]', '(50,75]', '(75,100]')
+try: 
+    valor = float(input())
+except ValueError: 
+    print(f"Somente números !!!, Digite novamente:") 
+    valor = float(input())
+
+if valor >= 0 and valor <= 25.0000000: print(f"Intervalo {intervalor_print[0]}")
+elif valor > 25.0000000 and valor <= 50.0000000: print(f"Intervalo {intervalor_print[1]}")
+elif valor > 50.0000000 and valor <= 75.0000000: print(f"Intervalo {intervalor_print[2]}")
+elif valor > 75.0000000 and valor <= 100.0000000: print(f"Intervalo {intervalor_print[3]}")
+else: print(f"Fora de intervalo")
